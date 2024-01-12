d69f9e8def8b99fb22916bee8ca01efef14d07d1
joao1306 <joaovictor1306@gmail.com>
2024-01-12 11:08:14 -0300

    about page layout

--- src/components/about/sobre.js
+++ src/components/about/sobre.js
@@ -1,8 +1,33 @@
-import React from 'react'
-import './sobre.css'
+import React from 'react';
+import './sobre.css';
+import blocoBranco from '../home/source/bloco.svg';
+import blocoLaranja from '../home/source/blocolaranja.svg';
 
 export default function Sobre() {
   return (
-    <div id='sobre'></div>
+    <div id='sobre'>
+      <p className='titulo-secao'>SOBRE <span class='negrito-laranja'>MIM</span></p>
+      <div id='bloco-sobre-mim'>
+        <div id='bloco-foto-sobre-mim'>
+          <img src={blocoBranco} id='bloco-branco'/>
+          <div id='linha-foto-sobre-mim'></div>
+          <div id='display-foto-sobre-mim'>           
+            <img src='https://media.istockphoto.com/id/866198538/pt/foto/glasses-guy.jpg?s=612x612&w=0&k=20&c=wHY_m4pfkAJla3WPtCOK_Wtkfi5nZsmJ4sNr0wI_UiQ=' id='foto-sobre-mim'></img>
+          </div>
+        </div>
+        <div id='bloco-texto-sobre-mim'>
+          <p id='titulo-bloco-texto-sobre-mim'>Is simply dummy text of the printing and typesetting industry.</p>
+          <p id='bloco-descricao-sobre-mim'>Cras ut erat semper, placerat lectusrisque aliquet. Etiam lacinia, sem commodo porttitor commodo, nibh velit hendrerit ipsum, eget efficitur enim lorem a sem. Fusce tempor diam nunc. Sed ultrices, arcu a facilisis imperdiet, odio mi vestibulum urna, in condimentum neque eros nec justo. Praesent sodales condimentum tellus. Proin commodo tempus tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus pharetra ante metus, eget eleifend ligula pretium non. Donec id nisi ex. Sed condimentum magna at magna dapibus commodo. Vestibulum eleifend, augue et vestibulum tincidunt, neque lacus facilisis lacus, vel venenatis erat metus ac magna. Sed id commodo nibh, nec rutrum nulla. Cras condimentum pulvinar nisl quis bibendum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce pharetra quam at tristique rhoncus.</p>
+          <div id='bloco-infos-sobre-mim'>
+            <div className='item-bloco-info'>1</div>
+            <div className='item-bloco-info'>2</div>
+            <div className='item-bloco-info'>3</div>
+            <div className='item-bloco-info'>4</div>
+          </div>
+          <div id='botao-curriculo'></div>
+        </div>
+      </div>
+      <img src={blocoLaranja} id='bloco-laranja'></img>
+    </div>
   )
 }
